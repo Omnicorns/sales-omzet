c6528feb846c1b233bdd6ab33058f8aef71a6804
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-07-01 10:48:08 +0700

    [IMPROVE] enhance builder default

--- src/main/java/com/sarinah/tenantsalesomzet/request/TenantOmzetDateRangeRequest.java
+++ src/main/java/com/sarinah/tenantsalesomzet/request/TenantOmzetDateRangeRequest.java
@@ -18,7 +18,10 @@ public class TenantOmzetDateRangeRequest {
     private Date startDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date endDate;
+    @Builder.Default
     private int page = 0;  // default halaman pertama
+
+    @Builder.Default
     private int size = 10; // default 10 data per halaman
 
 }
