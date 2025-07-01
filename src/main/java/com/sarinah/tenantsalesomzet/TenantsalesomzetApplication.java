34686907d33a86fba508e8101a31cba09402d974
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-07-01 14:13:31 +0700

    [IMPROVE] add annotation

--- src/main/java/com/sarinah/tenantsalesomzet/TenantsalesomzetApplication.java
+++ src/main/java/com/sarinah/tenantsalesomzet/TenantsalesomzetApplication.java
@@ -3,9 +3,13 @@ package com.sarinah.tenantsalesomzet;
 import com.sarinah.tenantsalesomzet.apisecurity.tokensecurity.annotation.EnableTokenSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableTokenSecurity
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class TenantsalesomzetApplication {
 
 	public static void main(String[] args) {
