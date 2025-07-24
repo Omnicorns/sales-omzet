c705529baf9c429832d42c5722d93076b7749525
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-07-24 12:10:02 +0700

    add custom languange

--- src/main/java/com/sarinah/tenantsalesomzet/service/ValidateTenantOmzetService.java
+++ src/main/java/com/sarinah/tenantsalesomzet/service/ValidateTenantOmzetService.java
@@ -9,6 +9,12 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.web.server.ResponseStatusException;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.List;
 
@@ -18,34 +24,30 @@ import static com.sarinah.tenantsalesomzet.util.Constant.ERR_MSG_AUTH_CLIENT_UNS
 
 @Service
 public class ValidateTenantOmzetService {
+    private static final String[] DATE_PATTERNS = {
+            "dd-MM-yyyy HH:mm:ss",
+            "dd-MM-yyyy HH:mm",
+            "dd-MM-yyyy"
+    };
     public ValidationResponse execute (PostTenantOmzetRequest input){
         this.doFilterReceiptList(input);
-
         return ValidationResponse.builder().result(true).build();
     }
-
-
 
     private void doFilterReceiptList(PostTenantOmzetRequest input) {
         List<ReceiptItem> receiptList = input.getReceiptList();
         if (receiptList == null || receiptList.isEmpty()) {
-            throw new BusinessException(ERROR_CODE_30000,"Receipt list cannot be empty");
+            throw new BusinessException(ERROR_CODE_30000, "Receipt list tidak boleh  kosong");
 
 
         }
         // (opsional) validasi tiap item:
         for (ReceiptItem r : receiptList) {
             if (r.getReceiptNumber() == null || r.getReceiptNumber().isBlank()) {
-                throw new BusinessException(ERROR_CODE_30000,"Receipt Number cannot be empty");
+                throw new BusinessException(ERROR_CODE_30000, "Receipt Number tidak boleh kosong");
             }
-            // ... validasi field lain jika perlu
-        }
+
     }
-
-
-
-
-
-
+    }
 
 }
